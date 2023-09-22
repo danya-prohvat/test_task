2d5f4acfa4ff57732d15205159c2e16ff8d68b70
daniil <prestig.rd85@gmail.com>
2023-09-22 15:15:29 +0300

    some refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+  constructor(
+    private router: Router,
+  ) {}
   title = 'angular_test';
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+
+    if (!token) this.router.navigate(['login']);
+  }
 }
